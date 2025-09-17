121b276c602b2253396a2fc8d1c3370b2914d280
Muhammad Fadhil Syahputra <2211014210002@mhs.ulm.ac.id>
2025-09-17 11:43:46 +0000

    feat: complete LoRa Util method implementation , make unit test

--- Lora/SX1276/internal/Util_test.go
+++ Lora/SX1276/internal/Util_test.go
@@ -10,7 +10,7 @@ func newLoraUtils() *LoraUtils {
 	return &LoraUtils{}
 }
 
-func TestSetWriteMask(t *testing.T) {
+func TestLoraUtils_SetWriteMask(t *testing.T) {
 	lu := newLoraUtils()
 	tests := []struct {
 		input byte
@@ -30,7 +30,7 @@ func TestSetWriteMask(t *testing.T) {
 	}
 }
 
-func TestSetReadMask(t *testing.T) {
+func TestLoraUtils_SetReadMask(t *testing.T) {
 	lu := newLoraUtils()
 	tests := []struct {
 		input byte
@@ -48,7 +48,7 @@ func TestSetReadMask(t *testing.T) {
 	}
 }
 
-func TestChangeMode(t *testing.T) {
+func TestLoraUtils_ChangeMode(t *testing.T) {
 	lu := newLoraUtils()
 	test := []struct {
 		name  string
@@ -88,4 +88,62 @@ func TestChangeMode(t *testing.T) {
 		})
 	}
 
+}
+
+func TestLoraUtils_SetFreq(t *testing.T) {
+	lu := newLoraUtils()
+	tests := []struct {
+		name string
+		freq uint64
+		want []byte
+	}{
+		{
+			name: "freq 0",
+			freq: 0,
+			want: []byte{0x00, 0x00, 0x00},
+		},
+		{
+			name: "freq kecil",
+			freq: 0x123456,
+			want: []byte{0x12, 0x34, 0x56},
+		},
+		{
+			name: "freq besar",
+			freq: 0xFFFFFF,
+			want: []byte{0xFF, 0xFF, 0xFF},
+		},
+		{
+			name: "freq lebih dari 24 bit",
+			freq: 0x12345678,
+			want: []byte{0x34, 0x56, 0x78}, // cuma ambil 3 byte terakhir
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := lu.SetFreq(tt.freq)
+			assert.Equal(t, tt.want, result)
+		})
+	}
+}
+
+func TestLoraUtils_SetTxPower(t *testing.T) {
+	lu := newLoraUtils()
+	tests := []struct {
+		power byte
+		want  byte
+	}{
+		{power: 0x00, want: 0x80},
+		{power: 0x01, want: 0x81},
+		{power: 0x02, want: 0x82},
+		{power: 0x03, want: 0x83},
+		{power: 0x7f, want: 0xff},
+	}
+
+	for _, tt := range tests {
+		t.Run("write Mask Test", func(t *testing.T) {
+			result := lu.SetWriteMask(tt.power)
+			assert.Equal(t, tt.want, result)
+		})
+	}
 }
